81f3f74cb6ee324fa3b2278ae58e4cb737ea2d5f
TimeTravellingMachine <149077424+TimeTravellingMachine@users.noreply.github.com>
2023-11-01 18:06:07 +0530

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -3,4 +3,4 @@ wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.
 unzip ngrok.zip &&
 chmod +x ngrok && pip install udocker
 ./ngrok authtoken 2WKx5P2XbH0xSOIpBhSa5pP8HL9_5B9CGxeS3wWX99Ltyd7yz &&
-./ngrok -- region us http 6080 & udocker --allow-root run -p 6080:80 dorowu/ubuntu-desktop-lxde-vnc
+./ngrok http 6080 & udocker --allow-root run -p 6080:80 dorowu/ubuntu-desktop-lxde-vnc
