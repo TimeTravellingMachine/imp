dfeca48888b677635eb7d61dfd7d11e352ae4281
TimeTravellingMachine <149077424+TimeTravellingMachine@users.noreply.github.com>
2023-11-01 17:41:39 +0530

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -1,28 +1,6 @@
-echo -e "$123\n$123\n" | sudo passwd
-rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/3sPrc9S > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
-clear
-./ngrok tcp --region in 3388 &>/dev/null &
-echo "===================================="
-echo "Install RDP"
-echo "===================================="
-udocker --root-allow pull danielguerra/ubuntu-xrdp
-clear
-echo "===================================="
-echo "Start RDP"
-echo "===================================="
-echo "===================================="
-echo "Username : ubuntu"
-echo "Password : ubuntu"
-echo "RDP Address:"
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "===================================="
-echo "Don't close this tab to keep RDP running"
-echo "Keep support HACKTECHTV CHANNEL thank you"
-echo "Wait 2 minute to finish the setup then Run using RDP Address"
-echo "===================================="
-echo "===================================="
-udocker --root-allow run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1
+#!/bin/bash
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip &&
+unzip ngrok.zip &&
+chmod +x ngrok &&
+./ngrok authtoken 2WKx5P2XbH0xSOIpBhSa5pP8HL9_5B9CGxeS3wWX99Ltyd7yz &&
+./ngrok http 6080 & docker run -p 6080:80 dorowu/ubuntu-desktop-lxde-vnc
