de45b3402f80ef3ed4f16cf9db7177ee57863b88
TimeTravellingMachine <149077424+TimeTravellingMachine@users.noreply.github.com>
2023-11-01 18:42:58 +0530

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -3,4 +3,6 @@ wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.
 unzip ngrok.zip &&
 chmod +x ngrok && pip install udocker
 ./ngrok authtoken 2WKx5P2XbH0xSOIpBhSa5pP8HL9_5B9CGxeS3wWX99Ltyd7yz &&
-./ngrok http 6080 & udocker --allow-root run -p 6080:80 dorowu/ubuntu-desktop-lxde-vnc
+./ngrok tcp 3388 & udocker --allow-root pull danielguerra/ubuntu-xrdp
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+docker --allow-root run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali
